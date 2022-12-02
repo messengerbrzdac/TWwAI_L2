1df61a71917f2a23752576a9e3514e788f4a90dd
messengerbrzdac <91937863+messengerbrzdac@users.noreply.github.com>
2022-12-02 12:59:04 +0100

    lab06
    add files to lab06

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const express = require('express');
-const config = require('./config').config;
-
-const questions = require('./questions').questions;
-const app = express();
-
-app.get('/api/questions', (request, response) => {
-    response.send(questions);
- });
-
-app.listen(config.port, function () {
- console.info(`Server is running at port 3000`);
-});
+require( 'babel-core/register' );
+require( './app');
